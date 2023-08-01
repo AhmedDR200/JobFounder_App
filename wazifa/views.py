f4aa04a7ddf57df729e43106ba68de01bc1b94d4
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-08-01 11:50:16 +0300

    views file-->(job list , job detail)

--- wazifa/views.py
+++ wazifa/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 
 
 
 
 def job_list(request):
-    pass
+    job_list = Job.objects.all() #get all jobs
+    return render(request, 'wazifa/job_list.html', {'jobs':job_list}) #--> template name
 
 
 def job_detail(request, id):
-    pass
+   job_detail = Job.objects.get(id=id) #get 1 job
+   return render(request, 'wazifa/job_detail.html', {'job':job_detail}) #--> template name

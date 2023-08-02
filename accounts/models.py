bde43683b9097b36ffb31b1587a94d49d712fc56
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-08-02 12:36:23 +0300

    SignUp model , django signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.ForeignKey("City", related_name='user_city' ,on_delete=models.CASCADE , null=True, blank=True)
+    phone_number = models.CharField(max_length=15)
+    image = models.ImageField(upload_to='profile/')
+
+    def __str__(self):
+      return str(self.user)
+
+
+# create new user ----> create empty profile
+# django signals
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+        
+
+
+
+
+class City(models.Model):
+     name = models.CharField(max_length=50)   

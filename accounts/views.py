89e06fea3bfaa4174b9055e5250996f3b566672e
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-08-02 13:35:48 +0300

    Profile & Profile_edit setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import authenticate , login
 from django.shortcuts import render , redirect
 from .forms import SignupForm
+from django.contrib.auth import authenticate , login
+from .models import Profile
 # Create your views here.
 
 
@@ -19,3 +20,15 @@ def signup(request):
         form=SignupForm()
 
     return render(request, 'registration/signup.html', {'form':form})
+
+
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'accounts/profile.html', {'profile':profile})
+
+
+
+def profile_edit(request):
+    return render(request, 'accounts/profile_edit.html', {})
